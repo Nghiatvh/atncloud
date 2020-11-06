876d43e7085ec919dcb4cc15908333d01d4e4152
nghiatvh <nghia17042001@gmail.com>
2020-11-06 22:12:25 +0700

    first commit

---
+++ configs/config_DB.js
@@ -0,0 +1,16 @@
+module.exports = {
+    localdb: {
+        urldb : 'mongodb+srv://nghiatvh:nghia123456@cluster0.c0xty.mongodb.net/atn-shop?retryWrites=true&w=majority',
+        dbname : "atn-shop",
+        dbusername : "nghiatvh",
+        dbpassword : "nghia123456",
+    },
+
+    clouddb: {
+        urldb : 'mongodb+srv://nghiatvh:nghia123456@cluster0.c0xty.mongodb.net/atn-shop?retryWrites=true&w=majority',
+        dbname : "atn-shop",
+        dbusername : "nghiatvh",
+        dbpassword : "nghia123456",
+    }
+
+};
